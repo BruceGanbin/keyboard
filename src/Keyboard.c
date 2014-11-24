5a7c87c44c8a445d914ee403c1b7475076ecc4c3
Bruce_gan <ckibn008@gmail.com>
2014-11-25 00:30:10 +0800

    Remove HT66FB550 code ,Add SH68F83 code base
    Build new project,and keys scan code
    
    Signed-off-by: Bruce_gan <ckibn008@gmail.com>

--- src/Keyboard.c
+++ src/Keyboard.c
@@ -1,399 +1,104 @@
-/*******************************************
-PC0-8: scan in
-PB0-8: scan out
-*******************************************/
-#include "ModifyCode.h"
 
-void usb_key_examine();
+#include "keyboard.h"
 
+static unsigned char keynum_dat[3][8];
 
-void always_no_fn_mode_now()
+static void scan_out_high(void)
 {
-	uchar8 R0,temp,bitbuff,int_temp2;
-	R0=Fn_key_break_state[int_temp1>>3]; //int_temp1/8
-	int_temp2=scan_out[int_temp1&0x07]; //int_temp1%8
-	temp=~int_temp2;
-	if(KeyFlag.fn_key_break==0)
-	{
-		if(KeyFlag.f2_f1)  //Fn+Fx
-		{
-			if(KeyFlag.Fn_mode) R0&=int_temp2;  //normal key
-			else R0|=temp; //function key
-		}
-		else //small num
-		{
-			if(KeyFlag.Fn_mode^KeyFlag.NumLockStatus) R0&=int_temp2;
-			else R0|=temp;
-		}
-		Fn_key_break_state[int_temp1>>3]=R0;
-//		int_temp1=0;
-//		_acc=0;
-//		if(KeyFlag.NumLock)int_temp1=0xff;
-//		if(KeyFlag.Fn_mode==0) _acc=0xff;
-//		if(KeyFlag.f2_f1==0) _acc^=int_temp1;
-//		if(_acc&0xff) R0|=int_temp2;
-//		else R0&=(~int_temp2);		  
-	}
-	int_temp1=KeyMatrix[IndexBuf]&0xff;
-	int_temp2=Fn_mode_key[int_temp1<<1]&0xff;
-	temp1_matrix=(Fn_mode_key[int_temp1<<1]>>8)&0xff;
-	if(KeyFlag.fn_mode_now) //1:F2---have function key,then judge Fn make or break
-	{
-		if((KeyFlag.always_fn)||((KeyFlag.always_fn==0)&&(R0&temp)))
-		{
-		temp=(int_temp1<<1)+1;
-		int_temp2=Fn_mode_key[temp];  //get function key code
-	    temp1_matrix=(Fn_mode_key[temp]>>8)&0xff;	
-		}
-	}
-//	exit_of_fn_key();
-    fn_key_code=int_temp2;
-    if(KeyFlag.has_usb_fn) 
-    {
-    KeyFlag.fn_key_break=0;
-    //usb_key_examine()
-	switch(temp1_matrix&0x0f)
-	{
-		case 0: keycode=fn_key_code;
-		        KeyFlag.return_value=0;break;
-//		case 3: if(CurrentState&0x01) KeyFlag.hotkeyset=1;
-//		        else KeyFlag.combinekey=1;
-//		        KeyFlag.return_value=0;break;
-		case 0x0c: KeyFlag.hotkeyset=1;
-		        if(CurrentState&0x01) KeyFlag.Fn_mode=1;
-		        else KeyFlag.Fn_mode=0;
-		        KeyFlag.return_value=1;break;
-		default:
-		{ 	
-			//bit_map_key()
-			KeyFlag.hotkeyset=1;
-			if(KeyFlag.has_usb_fn) int_temp1=fn_key_code;
-		    switch(temp1_matrix&0x0f)
-			{
-				case 0x04: KeyFlag.multi_key_chg=1;break; 
-				case 0x05: KeyFlag.multi_key_chg=1;break; 
-				case 0x06: KeyFlag.multi_key_chg=1;break;
-				case 0x07: KeyFlag.acpi_key_chg=1; break;
-				case 0x08: KeyFlag.KB_KeyUpt=1;    break;
-				case 0x09: KeyFlag.hot_key_chg=1;  break;
-			}
-			bitbuff=(temp1_matrix&0x0f)-4;
-		    if(CurrentState&0x01) key_byte[bitbuff]&=(~int_temp1);
-		    else key_byte[bitbuff]|=int_temp1;	
-		    KeyFlag.return_value=1;		
-		}break;		
-	}
-    }
- //   else ps2_key_examine(); 
-}
-void has_fn1_key()
-{
-	uint16 temp;
-	KeyFlag.fn_key_break=0;
-	if(CurrentState&0x01) KeyFlag.fn_key_break=1;
-	KeyFlag.has_usb_fn=1;
-	//examine_function_key
-	KeyFlag.f2_f1=0;
-	int_temp1=KeyMatrix[IndexBuf]&0xff;
-//	int_temp2=int_temp1;
-	//ps2_key_examine();
-	switch(temp1_matrix&0x0f)
-	{
-		case 1: KeyFlag.always_fn=0;
-		        KeyFlag.fn_mode_now=1; 
-		        always_no_fn_mode_now();break;//small num area  
-		case 2: KeyFlag.always_fn=0;
-		        KeyFlag.fn_mode_now=1;
-		        KeyFlag.f2_f1=1;
-		        always_no_fn_mode_now();break;//Fn+Fx
-	}	
-}    
-
-void UpdateState(uchar8 Key_No)
-{
-	if(CurrentState&0x01)
-    {
-    	GOLDKY[scanlineno]|=~scan_out[Key_No];
-    	if(KeyFlag.hotkeyset==0) 
-    	KeyFlag.KB_KeyUpt=1; 
-    }
-    else 
-    {
-    	GOLDKY[scanlineno]&=scan_out[Key_No];
-    	if(KeyFlag.hotkeyset==0) 
-    	KeyFlag.KB_KeyUpt=1;
-    }
-}    
-void chk_key_change()
-{
-	uchar8 i,temp,ChangeKeyMap,KeyNo,bitbuff;
-	 KeyFlag.chk_key_change=0;
-    for(scanlineno=0;scanlineno<total_scanline;scanlineno++)
-    {
-	CurrentState=GNEWKY[scanlineno];//makecode or breakcode
-	ChangeKeyMap=GNEWKY[scanlineno]^GOLDKY[scanlineno];
-	for(KeyNo=0;KeyNo<8;KeyNo++)
-	{	
-		if(ChangeKeyMap&0x01)
-		{
-			KeyFlag.has_key_change=1;
-			IndexBuf=(scanlineno<<3)+KeyNo;
-		    //translate_to_key_code();
-		    KeyFlag.hotkeyset=0; //hotkeyset:whether send data,0:send
-			KeyFlag.f2_f1=0;
-			KeyFlag.has_usb_fn=0;
-			KeyFlag.fn_key_break=0;
-			if(IndexBuf==FnKeyIndex)
-			{
-			if(CurrentState&0x01) KeyFlag.Fn_mode=1;
-		    else KeyFlag.Fn_mode=0;
-			}
-			
-			int_temp1=KeyMatrix[IndexBuf]&0xff;
-			temp1_matrix=(KeyMatrix[IndexBuf]>>8)&0xff;
-			//usb_key_examine()
-			switch(temp1_matrix&0x0f)
-			{
-				case 0: keycode=int_temp1;
-				        KeyFlag.return_value=0;break;
-				case 1:               //function key,Fn key
-				case 2: has_fn1_key();break; //Fn+Fx
-				case 3: KeyFlag.hotkeyset=1;
-				        if(Buff_Index==0)
-				        {
-				        if(CurrentState&0x01) KeyFlag.combinekey=0;
-				        else {KeyFlag.combinekey=1;KeyFlag.KB_KeyUpt=1;}
-				        comkeys=0;
-				        }
-				        KeyFlag.return_value=1;break;
-				case 0x0c: KeyFlag.hotkeyset=1;
-				           KeyFlag.return_value=1;break;
-				default:
-				{ 	
-					//bit_map_key()
-					KeyFlag.hotkeyset=1;
-					if(KeyFlag.has_usb_fn) int_temp1=fn_key_code;
-				    switch(temp1_matrix&0x0f)
-					{
-						case 0x04: KeyFlag.multi_key_chg=1;break; 
-						case 0x05: KeyFlag.multi_key_chg=1;break; 
-						case 0x06: KeyFlag.multi_key_chg=1;break;
-						case 0x07: KeyFlag.acpi_key_chg=1; break;
-						case 0x08: KeyFlag.KB_KeyUpt=1;    break;
-						case 0x09: KeyFlag.hot_key_chg=1;  break;
-					}
-					bitbuff=(temp1_matrix&0x0f)-4;
-				    if(CurrentState&0x01) key_byte[bitbuff]&=(~int_temp1);
-				    else key_byte[bitbuff]|=int_temp1;	
-				    KeyFlag.return_value=1;		
-				}break;		
-			}	
-			
-			if(KeyFlag.return_value) 
-			{
-                UpdateState(KeyNo);
-			}
-			else    //0:normal key,reordering buffer
-			{
-				if(CurrentState&0x01)
-				{
-				    //user_remove_sort();
-                    for(i=0;i<Buff_Index;i++)
-                    {
-                    	if(key_code_buff[i]==keycode)
-                    	{
-                    		key_code_buff[i]=key_code_buff[i+1];
-                    		key_code_buff[i+1]=keycode;
-                    	}
-                    }
-                    key_code_buff[Buff_Index]=0;
-                    Buff_Index--;
-				    KeyFlag.buffull=0;
-//				    _usbe=1;
-				    UpdateState(KeyNo);	
-				}
-			    else
-			    {			    	
-			    	if(Buff_Index==KEY_BUF_END)
-			    	{
-//			    		_usbe=1;
-			    		if(KeyFlag.buffull==0)  
-			    		{
-			    			KeyFlag.buffull=1;
-			    			KeyFlag.KB_KeyUpt=1;			    		
-			    		}
-			    	}
-			    	else 
-			    	{		    		
-			    		key_code_buff[Buff_Index]=keycode;
-			    		Buff_Index++;
-			    		KeyFlag.buffull=0;
-//			    		_usbe=1;
-			    		UpdateState(KeyNo);
-			    	}
-			    } 
-			}			
-		}
-		CurrentState>>=1;
-		ChangeKeyMap>>=1;
-	}
-    }
-    if(KeyFlag.has_change)
-    {
-    	KeyFlag.has_change=0;
-    	if(KeyFlag.buffull && KeyFlag.has_key_change==0)
-    	{
-    		KeyFlag.buffull=0;
-    		KeyFlag.KB_KeyUpt=1;
-    	}
-    }
-}
-void scan_out_high()
-{
-	_pcc|=0xff;
-	_pc |=0xff;	   //scan in
-	
-	_pac|=0xff;
-	_pa |=0xff;
-	_pbc|=0xff;  
-	_pb |=0xff;
-	_pdc|=0xe3;    //pd2\3\4 for num\cap\scr
-	_pd |=0xe3;    //scan out
-	
-	_pec|=0xff;
-	_pe |=0xff;	
-}
-void set_led()
-{
-	if(KeyFlag.NumLockStatus)
-	{
-		NumLockC=0;
-		NumLock=0;
-	}
-	if(KeyFlag.CapsLockStatus)
-	{
-		CapsLockC=0;
-		CapsLock=0;
-	}
-	if(KeyFlag.ScrollLockStatus)
-	{
-		ScrollLockC=0;
-		ScrollLock=0;
-	}
-}
-void MKYSCN()
-{
-//	const uchar8 *p_scan;//
-    volatile uchar8 *p_scan;
-    uchar8 i,scanout,temp,ChgKeyState,bitcnt;
-	for(scanlineno=0;scanlineno<total_scanline;scanlineno++)
-	{
-		_usbe=0;
-		scan_out_high();
-		scanout=scan_line_tab[scanlineno];
-		temp=(scanout&0xf0)>>4;   //get IO_address offset
-		p_scan=IO_address[temp];
-		*(p_scan)=scan_out[scanout&0x0f];   //for scan out control
-		temp++;
-		p_scan=IO_address[temp];
-		*(p_scan)=scan_out[scanout&0x0f];   //for scan out
-//		_usbe=1;
-		GCC_CLRWDT();   //delay us;
-		p_scan=IO_address[1];  //scan in
-		temp=*(p_scan);
-		if(temp!=GNEWKY[scanlineno])
-		{
-            GKEYCT=0;
-	//		temp=*p_scan;
-			GNEWKY[scanlineno]=temp;
-			KeyFlag.has_change=1;
-		}
-		scan_out_high();
-		set_led();	
-		if(scanlineno==0) GPHONT=~GNEWKY[0];
-		else 
-		{
-			temp=~GNEWKY[scanlineno];
-			ChgKeyState=temp&GPHONT;
-			GPHONT|=temp;	
-		}
-		bitcnt=0;
-		for(i=0;i<8;i++)
-		{
-		    if(ChgKeyState&0x01) bitcnt++;
-		    ChgKeyState>>=1;
-		}
-		if(bitcnt>=2)
-		{
-			KeyFlag.FPHNCK=1;
-			if(KeyFlag.FPHONK==0)
-			{
-				KeyFlag.FPHONK=1;
-				GREPCT=45;
-			}
-		}
-	}
-	if(KeyFlag.FPHNCK==0)
-	{
-		if(KeyFlag.FPHONK)
-		{
-			KeyFlag.FPHONK=0;           //phantom key relieve
-			if(KeyFlag.thefirstPHout)
-			KeyFlag.KB_KeyUpt=1;               
-			KeyFlag.thefirstPHout=0;
-		}			
-	}
-	KeyFlag.FPHNCK=0;
-	GPHONT=0;
-	if(++GKEYCT==4)
-	{
-		GKEYCT=0;		
-		KeyFlag.has_key_change=0;
-		if(KeyFlag.FPHONK)
-		{
-			if(--GREPCT==0)
-			{
-				GREPCT=45;
-				if(KeyFlag.thefirstPHout==0) 
-				KeyFlag.KB_KeyUpt=1;           //have phantom key
-				KeyFlag.thefirstPHout=1;
-			}
-		}
-//		else KeyFlag.chk_key_change=1;
-        else chk_key_change();
-	}	
+    PGIO_ALL_HIGH();
 }
 
+static void keyscan(void)
+{
+    unsigned char i,j;
+    unsigned char cycle_ctr=OxFE;
+    unsigned int ret;
+    unsigned char keynum[3];
+    unsigned char temp[3];
     
+    scan_out_high();
+    GPIO_COL = cycle_ctr;
+    for(i=0;i<8;i++)
+    {
+        temp[0] = GPIO_ROW1;
+        temp[1] = GPIO_ROW2;
+        temp[2] = GPIO_ROW3;
+        delay(20);
+        keynum[0] = GPIO_ROW1;
+        keynum[1] = GPIO_ROW2;
+        keynum[3] = GPIO_ROW3;
+        if(keynum[0]!=temp[0] || keynum[1]!=temp[1] || keynum[2]!=temp[2]) 
+        {
+            keynum_dat[0][i] = 0xFF;
+            keynum_dat[1][i] = 0xFF;
+            keynum_dat[2][i] = 0xFF;
+            continue;
+        }
+        keynum_dat[0][i] = keynum[0];
+        keynum_dat[1][i] = keynum[1];
+        keynum_dat[3][i] = keynum[2];
+        
+        cycle_ctr = _crol_(cycle_ctr,1);
+        GPIO_COL = cycle_ctr;
+    }
+
+}
+
+extern usbkeydat[8];
+void readscan(void);
+{
+
+    unsigned char i,j,k;
+    unsigned char press_key = 2;
+    unsigned char key_value = 0;
+    for(i=0;i<8;i++)
+        usbkeydat[i] = 0;
     
+    keyscan();
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<8;j++)
+        {
+            if(keynum[i][j] == 0xFF)  // no key press down
+                continue;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            for(k=0;k<8;k++)
+            {
+                
+            }
+            
+            switch(keynum[i][j])
+            {
+            case PRESS_L_CTRL: usbkeydat[0] = usbkeydat[0] | KEY_L_CTRL;
+                break;
+            case PRESS_L_SHIFT: usbkeydat[0] = usbkeydat[0] | KEY_L_SHIFT;
+                break;
+            case PRESS_L_ALT: usbkeydat[0] = usbkeydat[0] | KEY_L_ALT;
+                break;
+            case PRESS_L_GUI:usbkeydat[0] = usbkeydat[0] | KEY_L_GUI ;
+                break;
+            case PRESS_R_CTRL: usbkeydat[0] = usbkeydat[0] | KEY_R_CTRL;
+                break;
+            case PRESS_R_SHIFT: usbkeydat[0] = usbkeydat[0] | KEY_R_SHIFT;
+                break;
+            case PRESS_R_ALT: usbkeydat[0] = usbkeydat[0] | KEY_R_ALT;
+                break;
+            case PRESS_R_GUI:usbkeydat[0] = usbkeydat[0] | KEY_R_GUI ;
+                break;
+            default:
+                usbkeydat[press_key] = key_value;
+                break;
+            }
+            
+            press_key++;
+            if(press_key > 7)
+            {
+                for(k=0;k<8;k++)
+                    usbkeydat[i] = 0;
+                return;
+            }
+        }
+    }
+}
 
--- src/Keyboard.h
+++ src/Keyboard.h
@@ -1,75 +1,88 @@
+#ifndef  __KEYBOARD_H
+#define  __KEYBOARD_H
 
-#ifndef  _KEYBOARD_H
-#define  _KEYBOARD_H
+#include <sh68F83.h>
+#include <intrins.h>
 
-#define   total_scanline   18
-#define   KEY_BUF_END      0x06
-#define   FnKeyIndex       0x61     
+sbit   R0 = P;
+sbit   R1 = P;    
+sbit   R2 = P;
+sbit   R3 = P;
+sbit   R4 = P;
+sbit   R5 = P;
+sbit   R6 = P;
+sbit   R7 = P;
 
-#define   s0_scan_out      0x40
-#define   s1_scan_out      0x41
-#define   s2_scan_out      0x42
-#define   s3_scan_out      0x43
-#define   s4_scan_out      0x44
-#define   s5_scan_out      0x45
-#define   s6_scan_out      0x46
-#define   s7_scan_out      0x47
-#define   s8_scan_out      0x20
-#define   s9_scan_out      0x21
-#define   s10_scan_out     0x22
-#define   s11_scan_out     0x23
-#define   s12_scan_out     0x24
-#define   s13_scan_out     0x25
-#define   s14_scan_out     0x26
-#define   s15_scan_out     0x27
-#define   s16_scan_out     0x60
-#define   s17_scan_out     0x61
+sbit   C0 = p;
+sbit   C1 = p;
+sbit   C2 = p;
+sbit   C3 = p;
+sbit   C4 = p;
+sbit   C5 = p;
+sbit   C6 = p;
+sbit   C7 = p;
+sbit   C8 = p;
+sbit   C9 = p;
+sbit   C10 = p;
+sbit   C11 = p;
+sbit   C12 = p;
+sbit   C13 = p;
+sbit   C14 = p;
+sbit   C15 = p;
+sbit   C16 = p;
 
-#define   key_error        0x01
-#define   key_undefine     0x03
+#defind GPIO_ALL_HIGH()    P0=0xFF;P1=0xFF;P2=0xFF
+#define GPIO_COL           P2
+#define GOPI_ROW1          P1
+#define GPIO_ROW2          P3
+#define GPIO_ROW3          P0&0x40
 
-#define   KEY_A            0x04
-#define   KEY_B            0x05
-#define   KEY_C            0x06
-#define   KEY_D            0x07
-#define   KEY_E            0x08
-#define   KEY_F            0x09
-#define   KEY_G            0x0a
-#define   KEY_H            0x0b
-#define   KEY_I            0x0c
-#define   KEY_J            0x0d
-#define   KEY_K            0x0e
-#define   KEY_L            0x0f
-#define   KEY_M            0x10
-#define   KEY_N            0x11
-#define   KEY_O            0x12
-#define   KEY_P            0x13
-#define   KEY_Q            0x14
-#define   KEY_R            0x15
-#define   KEY_S            0x16
-#define   KEY_T            0x17
-#define   KEY_U            0x18
-#define   KEY_V            0x19
-#define   KEY_W            0x1a
-#define   KEY_X            0x1b
-#define   KEY_Y            0x1c
-#define   KEY_Z            0x1d
-
-#define   KEY_ESCAPE       0x29       
-#define   KEY_Tab          0x2b
-#define   KEY_CAPSLOCK     0x39
-#define   KEY_Backspace    0x2a
-#define   KEY_ENTER        0x28
-#define   KEY_SPACEBAR     0x2c
-
-#define   KEY_L_CTRL       0x0801//0xA0
-#define   KEY_L_SHIFT      0x0802//0xA1
-#define   KEY_L_ALT        0x0804//0xA2
-#define   KEY_L_GUI        0x0808//0xA3
-#define   KEY_R_CTRL       0x0810//0xA4
-#define   KEY_R_SHIFT      0x0820//0xA5
-#define   KEY_R_ALT        0x0840//0xA6
-#define   KEY_R_GUI        0x0880//0xA7
+#define   KEY_BUF_END         0x06
+#define   FnKeyIndex          0x61     
+                            
+                            
+#define   KEY_A               0x04
+#define   KEY_B               0x05
+#define   KEY_C               0x06
+#define   KEY_D               0x07
+#define   KEY_E               0x08
+#define   KEY_F               0x09
+#define   KEY_G               0x0a
+#define   KEY_H               0x0b
+#define   KEY_I               0x0c
+#define   KEY_J               0x0d
+#define   KEY_K               0x0e
+#define   KEY_L               0x0f
+#define   KEY_M               0x10
+#define   KEY_N               0x11
+#define   KEY_O               0x12
+#define   KEY_P               0x13
+#define   KEY_Q               0x14
+#define   KEY_R               0x15
+#define   KEY_S               0x16
+#define   KEY_T               0x17
+#define   KEY_U               0x18
+#define   KEY_V               0x19
+#define   KEY_W               0x1a
+#define   KEY_X               0x1b
+#define   KEY_Y               0x1c
+#define   KEY_Z               0x1d
+                            
+#define   KEY_ESCAPE          0x29       
+#define   KEY_Tab             0x2b
+#define   KEY_CAPSLOCK        0x39
+#define   KEY_Backspace       0x2a
+#define   KEY_ENTER           0x28
+#define   KEY_SPACEBAR        0x2c
+                            
+#define   KEY_L_CTRL          0x01//0xA0
+#define   KEY_L_SHIFT         0x02//0xA1
+#define   KEY_L_ALT           0x04//0xA2
+#define   KEY_L_GUI           0x08//0xA3
+#define   KEY_R_CTRL          0x10//0xA4
+#define   KEY_R_SHIFT         0x20//0xA5
+#define   KEY_R_ALT           0x40//0xA6
+#define   KEY_R_GUI           0x80//0xA7
 
 #define   KEY_TILDE           0x35   //bo lang
 #define   KEY_Underscore      0x2d   //xia hua xian
@@ -83,29 +96,30 @@
 #define   KEY_PERIOD          0x37   //ju hao
 #define   KEY_Interrogation   0x38   //dun hao
 
-#define   KEY_1          0x1e
-#define   KEY_2          0x1f
-#define   KEY_3          0x20
-#define   KEY_4          0x21
-#define   KEY_5          0x22
-#define   KEY_6          0x23
-#define   KEY_7          0x24
-#define   KEY_8          0x25
-#define   KEY_9          0x26
-#define   KEY_0          0x27
+#define   KEY_1               0x1e
+#define   KEY_2               0x1f
+#define   KEY_3               0x20
+#define   KEY_4               0x21
+#define   KEY_5               0x22
+#define   KEY_6               0x23
+#define   KEY_7               0x24
+#define   KEY_8               0x25
+#define   KEY_9               0x26
+#define   KEY_0               0x27
+                              
+#define   KEY_F1              0x3a
+#define   KEY_F2              0x3b
+#define   KEY_F3              0x3c
+#define   KEY_F4              0x3d
+#define   KEY_F5              0x3e
+#define   KEY_F6              0x3f
+#define   KEY_F7              0x40
+#define   KEY_F8              0x41
+#define   KEY_F9              0x42
+#define   KEY_F10             0x43
+#define   KEY_F11             0x44
+#define   KEY_F12             0x45
 
-#define   KEY_F1         0x3a
-#define   KEY_F2         0x3b
-#define   KEY_F3         0x3c
-#define   KEY_F4         0x3d
-#define   KEY_F5         0x3e
-#define   KEY_F6         0x3f
-#define   KEY_F7         0x40
-#define   KEY_F8         0x41
-#define   KEY_F9         0x42
-#define   KEY_F10        0x43
-#define   KEY_F11        0x44
-#define   KEY_F12        0x45
          
 #define   KEY_PrintScreen      0x46
 #define   KEY_ScrollLock       0x47
@@ -130,112 +144,31 @@
 #define   KEY_NUM_ENTER        0x58
 #define   KEY_NUM_DOT          0x63
 
-#define   KEY_NUM_1       0x59
-#define   KEY_NUM_2       0x5a
-#define   KEY_NUM_3       0x5b
-#define   KEY_NUM_4       0x5c            
-#define   KEY_NUM_5       0x5d
-#define   KEY_NUM_6       0x5e
-#define   KEY_NUM_7       0x5f
-#define   KEY_NUM_8       0x60
-#define   KEY_NUM_9       0x61
-#define   KEY_NUM_0       0x62
+#define   KEY_NUM_1            0x59
+#define   KEY_NUM_2            0x5a
+#define   KEY_NUM_3            0x5b
+#define   KEY_NUM_4            0x5c            
+#define   KEY_NUM_5            0x5d
+#define   KEY_NUM_6            0x5e
+#define   KEY_NUM_7            0x5f
+#define   KEY_NUM_8            0x60
+#define   KEY_NUM_9            0x61
+#define   KEY_NUM_0            0x62
+                             
+#define   KEY_CODE29           0x31
+#define   KEY_CODE42           0x32
+#define   KEY_CODE45           0x64
+#define   KEY_APPS             0x65
+#define   KEY_CODE107          0x1085
+#define   KEY_CODE56           0x1087
+#define   KEY_CODE133          0x1088
+#define   KEY_CODE14           0x1089
+#define   KEY_CODE132          0x108a
+#define   KEY_CODE131          0x108b
+#define   KEY_CODE151          0x2090   //key_Hangul
+#define   KEY_CODE150          0x2091   //Key_Hanja
 
-#define   KEY_CODE29      0x31
-#define   KEY_CODE42      0x32
-#define   KEY_CODE45      0x64
-#define   KEY_APPS        0x65
-#define   KEY_CODE107     0x1085
-#define   KEY_CODE56      0x1087
-#define   KEY_CODE133     0x1088
-#define   KEY_CODE14      0x1089
-#define   KEY_CODE132     0x108a
-#define   KEY_CODE131     0x108b
-#define   KEY_CODE151     0x2090   //key_Hangul
-#define   KEY_CODE150     0x2091   //Key_Hanja
 
-//#define   KEY_FALSE       0x00     //for hot key
-
-#define   KEY_NextTr      0x4401//0xE8
-#define   KEY_PrevTr      0x4402//0xE9
-#define   KEY_Stop        0x4404//0xEA
-#define   KEY_Eject       0x4408//0xEB
-#define   KEY_PlayPause   0x4410//0xEC
-#define   KEY_Mute        0x4420//0xED
-#define   KEY_VolumI      0x0440//0xEE  //+
-#define   KEY_VolumD      0x0480//0xEF  //-
-
-#define   KEY_Media       0x4501//F0
-#define   KEY_Email       0x4502//F1
-#define   KEY_Calculator  0x4504//F2
-#define   KEY_MyComputer  0x4508//F3
-#define   KEY_Search      0x4510//F4
-#define   KEY_WWW         0x4520//F5
-#define   KEY_Back        0x4540//F6
-#define   KEY_Forward     0x4580//F7
-
-#define   KEY_iStop       0x4601//0xF8
-#define   KEY_Refresh     0x4602//0xF9
-#define   KEY_Favorites   0x4604//0xFA
-
-#define   KEY_Power       0x0701//0xA8
-#define   KEY_Sleep       0x0702//0xA9
-#define   KEY_WakeUp      0x0704//0xAA
-
-#define   KEY_User1       0x4901//0xC8
-#define   KEY_User2       0x4902//0xC9
-#define   KEY_User3       0x4904//0xCA
-#define   KEY_User4       0x4908//0xCB
-#define   KEY_User5       0x4910//0xCC
-#define   KEY_User6       0x4920//0xCD
-#define   KEY_User7       0x4940//0xCE
-#define   KEY_User8       0x4980//0xCF
-
-#define   KEY_Fn          0x0c00
-
-#define   KEY_User5_Fn         0x0200
-#define   KEY_Scr_Num          0x0101
-#define   KEY_mode1_7          0x0102
-#define   KEY_mode1_8          0x0103
-#define   KEY_mode1_9          0x0104
-#define   KEY_mode1_0          0x0105
-#define   KEY_mode1_U          0x0106
-#define   KEY_mode1_I          0x0107
-#define   KEY_mode1_O          0x0108
-#define   KEY_mode1_P          0x0109
-#define   KEY_mode1_J          0x010A
-#define   KEY_mode1_K          0x010B
-#define   KEY_mode1_L          0x010C
-#define   KEY_mode1_Semicolon  0x010D
-#define   KEY_mode1_M          0x010E
-#define   KEY_mode1_COMMA      0x010F
-#define   KEY_mode1_PERIOD     0x0110
-
-#define   KEY_mode2_F1         0x0211
-#define   KEY_mode2_F2         0x0212
-#define   KEY_mode2_F3         0x0213
-#define   KEY_mode2_F4         0x0214
-#define   KEY_mode2_F5         0x0215
-#define   KEY_mode2_F6         0x0216
-#define   KEY_mode2_F7         0x0217
-#define   KEY_mode2_F8         0x0218
-#define   KEY_mode2_F9         0x0219
-#define   KEY_mode2_F10        0x021a
-#define   KEY_mode2_F11        0x021b
-#define   KEY_mode2_F12        0x021c
-
-//for combine key
-#define   key_ctrl_c	  0x4300
-#define   key_ctrl_x	  0x4301
-#define   key_ctrl_v	  0x4302
-
-//#define   Fn_mode         key_undefine
+void readscan(void);
 
 #endif
-
-
-
-
-
-
-
