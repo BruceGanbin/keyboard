bd367684daf07c3dbbe8765917c429fc3af8f717
Bruce_gan <ckbin008@gmail.com>
2014-11-26 21:47:12 +0800

    save
    Signed-off-by: Bruce_gan <ckbin008@gmail.com>

--- src/Keyboard.c
+++ src/Keyboard.c
@@ -47,45 +47,54 @@ void readscan(void);
     unsigned char i,j,k;
     unsigned char press_key = 2;
     unsigned char key_value = 0;
+    usnigned char circbit = 0;
+    unsigned char keybit = 0;
+    
     for(i=0;i<8;i++)
         usbkeydat[i] = 0;
     
     keyscan();
     
-    for(i=0;i<3;i++)
+    for(j=0;j<8;j++)
     {
-        for(j=0;j<8;j++)
+        for(i=0;i<3;i++)
         {
             if(keynum[i][j] == 0xFF)  // no key press down
                 continue;
 
+            circbit = 0x80;
             for(k=0;k<8;k++)
             {
-                
-            }
+                keybit = keynum[i][j] & circbit;
+                circbit = _cror_(circbit,1);
             
-            switch(keynum[i][j])
-            {
-            case PRESS_L_CTRL: usbkeydat[0] = usbkeydat[0] | KEY_L_CTRL;
-                break;
-            case PRESS_L_SHIFT: usbkeydat[0] = usbkeydat[0] | KEY_L_SHIFT;
-                break;
-            case PRESS_L_ALT: usbkeydat[0] = usbkeydat[0] | KEY_L_ALT;
-                break;
-            case PRESS_L_GUI:usbkeydat[0] = usbkeydat[0] | KEY_L_GUI ;
-                break;
-            case PRESS_R_CTRL: usbkeydat[0] = usbkeydat[0] | KEY_R_CTRL;
-                break;
-            case PRESS_R_SHIFT: usbkeydat[0] = usbkeydat[0] | KEY_R_SHIFT;
-                break;
-            case PRESS_R_ALT: usbkeydat[0] = usbkeydat[0] | KEY_R_ALT;
-                break;
-            case PRESS_R_GUI:usbkeydat[0] = usbkeydat[0] | KEY_R_GUI ;
-                break;
-            default:
-                usbkeydat[press_key] = key_value;
-                break;
-            }
+                if(keybit == 0)
+                    continue;
+
+                key_value = kyb_code[i][j];
+                
+                switch(circbit)
+                    {
+                    case PRESS_L_CTRL: usbkeydat[0] = usbkeydat[0] | KEY_L_CTRL;
+                        break;
+                    case PRESS_L_SHIFT: usbkeydat[0] = usbkeydat[0] | KEY_L_SHIFT;
+                        break;
+                    case PRESS_L_ALT: usbkeydat[0] = usbkeydat[0] | KEY_L_ALT;
+                        break;
+                    case PRESS_L_GUI:usbkeydat[0] = usbkeydat[0] | KEY_L_GUI ;
+                        break;
+                    case PRESS_R_CTRL: usbkeydat[0] = usbkeydat[0] | KEY_R_CTRL;
+                        break;
+                    case PRESS_R_SHIFT: usbkeydat[0] = usbkeydat[0] | KEY_R_SHIFT;
+                        break;
+                    case PRESS_R_ALT: usbkeydat[0] = usbkeydat[0] | KEY_R_ALT;
+                        break;
+                    case PRESS_R_GUI:usbkeydat[0] = usbkeydat[0] | KEY_R_GUI ;
+                        break;
+                    default:
+                        usbkeydat[press_key] = key_value;
+                        break;
+                    }
             
             press_key++;
             if(press_key > 7)
@@ -93,6 +102,8 @@ void readscan(void);
                 for(k=0;k<8;k++)
                     usbkeydat[i] = 0;
                 return;
+            }
+
             }
         }
     }
