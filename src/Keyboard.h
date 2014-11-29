577e02c9f4813e84e61a5ce0bac23981d51846d6
Bruce_gan <ckibn008@gmail.com>
2014-11-29 11:29:30 +0800

    Add key code into key_code array
    Signed-off-by: Bruce_gan <ckibn008@gmail.com>

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -37,9 +37,11 @@ sbit   C16 = p;
 #define GPIO_ROW2          P3
 #define GPIO_ROW3          P0&0x40
 
-#define   KEY_BUF_END         0x06
+
+
+
+#define   KEY_BUF_END         0x06 
 #define   FnKeyIndex          0x61     
-                            
                             
 #define   KEY_A               0x04
 #define   KEY_B               0x05
@@ -85,7 +87,7 @@ sbit   C16 = p;
 #define   KEY_R_GUI           0x80//0xA7
 
 #define   KEY_TILDE           0x35   //bo lang
-#define   KEY_Underscore      0x2d   //xia hua xian
+#define   KEY_Underscore      0x2d   //xia hua xiann
 #define   KEY_EqualSign       0x2e   //deng hao
 #define   KEY_L_Brackets      0x2f   //zuo zhong kuo hao
 #define   KEY_R_Brackets      0x30   //you zhong kuo hao
@@ -169,15 +171,94 @@ sbit   C16 = p;
 #define   KEY_CODE150          0x2091   //Key_Hanja
 
 
+#define   KEY_NextTr      0x4401//0xE8
+#define   KEY_PrevTr      0x4402//0xE9
+#define   KEY_Stop        0x4404//0xEA
+#define   KEY_Eject       0x4408//0xEB
+#define   KEY_PlayPause   0x4410//0xEC
+#define   KEY_Mute        0x4420//0xED
+#define   KEY_VolumI      0x0440//0xEE  //+
+#define   KEY_VolumD      0x0480//0xEF  //-
+
+#define   KEY_Media       0x4501//F0
+#define   KEY_Email       0x4502//F1
+#define   KEY_Calculator  0x4504//F2
+#define   KEY_MyComputer  0x4508//F3
+#define   KEY_Search      0x4510//F4
+#define   KEY_WWW         0x4520//F5
+#define   KEY_Back        0x4540//F6
+#define   KEY_Forward     0x4580//F7
+
+#define   KEY_iStop       0x4601//0xF8
+#define   KEY_Refresh     0x4602//0xF9
+#define   KEY_Favorites   0x4604//0xFA
+
+#define   KEY_Power       0x0701//0xA8
+#define   KEY_Sleep       0x0702//0xA9
+#define   KEY_WakeUp      0x0704//0xAA
+
+#define   KEY_User1       0x4901//0xC8
+#define   KEY_User2       0x4902//0xC9
+#define   KEY_User3       0x4904//0xCA
+#define   KEY_User4       0x4908//0xCB
+#define   KEY_User5       0x4910//0xCC
+#define   KEY_User6       0x4920//0xCD
+#define   KEY_User7       0x4940//0xCE
+#define   KEY_User8       0x4980//0xCF
+
+#define   KEY_Fn          0x0c00
+
+#define   KEY_User5_Fn         0x0200
+#define   KEY_Scr_Num          0x0101
+#define   KEY_mode1_7          0x0102
+#define   KEY_mode1_8          0x0103
+#define   KEY_mode1_9          0x0104
+#define   KEY_mode1_0          0x0105
+#define   KEY_mode1_U          0x0106
+#define   KEY_mode1_I          0x0107
+#define   KEY_mode1_O          0x0108
+#define   KEY_mode1_P          0x0109
+#define   KEY_mode1_J          0x010A
+#define   KEY_mode1_K          0x010B
+#define   KEY_mode1_L          0x010C
+#define   KEY_mode1_Semicolon  0x010D
+#define   KEY_mode1_M          0x010E
+#define   KEY_mode1_COMMA      0x010F
+#define   KEY_mode1_PERIOD     0x0110
+
+#define   KEY_mode2_F1         0x0211
+#define   KEY_mode2_F2         0x0212
+#define   KEY_mode2_F3         0x0213
+#define   KEY_mode2_F4         0x0214
+#define   KEY_mode2_F5         0x0215
+#define   KEY_mode2_F6         0x0216
+#define   KEY_mode2_F7         0x0217
+#define   KEY_mode2_F8         0x0218
+#define   KEY_mode2_F9         0x0219
+#define   KEY_mode2_F10        0x021a
+#define   KEY_mode2_F11        0x021b
+#define   KEY_mode2_F12        0x021c
+
+// const unsigned char kyb_code[8][17]={
+//     {0x00,0x00,0x00,'16','17','5' ,'20','23','10','11','12','13','1 ',0x00,0x00},
+//     {0x00,0x00,'44','30','18','19','21','7' ,'24','25','26','27','28',0x00,'29'},
+//     {'59',0x00,0x00,'46','31','33','34','37','38','39','40','41','43',0x00,'129'},
+//     {0x00,'127',0x00,0x00,'32','48','49','51','52','53','54','55',0x00,0x00,0x00},
+//     {0x00,0x00,0x00,'36',0x00,0x00,'22','50',0x00,0x00,'79','57','84','62','89'},
+//     {0x00,0x00,0x00,'58','47',0x00,'115','35',0x00,0x00,'61',0x00,0x00,'60','15'},
+//     {0x00,0x00,'83','2' ,'3' ,'4' ,'6' ,'8' ,'9','119','121','123','124',0x00,'76'},
+//     {'64',0x00,0x00,'110','112','113','114','116','117','118','120','122','126',0x00,'75'}
+// };
+
 const unsigned char kyb_code[8][17]={
-    {0x00,0x00,0x00,'16','17','5' ,'20','23','10','11','12','13','1 ',0x00,0x00},
-    {0x00,0x00,'44','30','18','19','21','7' ,'24','25','26','27','28',0x00,'29'},
-    {'59',0x00,0x00,'46','31','33','34','37','38','39','40','41','43',0x00,'129'},
-    {0x00,'127',0x00,0x00,'32','48','49','51','52','53','54','55',0x00,0x00,0x00},
-    {0x00,0x00,0x00,'36',0x00,0x00,'22','50',0x00,0x00,'79','57','84','62','89'},
-    {0x00,0x00,0x00,'58','47',0x00,'115','35',0x00,0x00,'61',0x00,0x00,'60','15'},
-    {0x00,0x00,'83','2' ,'3' ,'4' ,'6' ,'8' ,'9','119','121','123','124',0x00,'76'},
-    {'64',0x00,0x00,'110','112','113','114','116','117','118','120','122','126',0x00,'75'}
+    {0x00,0x00,0x00,KEY_Tab,KEY_Q,KEY_4,KEY_R,KEY_U,KEY_9,KEY_0,KEY_Underscore,KEY_EqualSign,KEY_TILDE,0x00,0x00},
+    {0x00,0x00,KEY_L_SHIFT,KEY_CAPSLOCK,KEY_W,KEY_E,KEY_T,KEY_6,KEY_I,KEY_O,KEY_P,KEY_L_Brackets,KEY_R_Brackets,0x00,KEY_Slash},
+    {FnKeyIndex,0x00,0x00,KEY_Z,KEY_A,KEY_D,KEY_F,KEY_J,KEY_K,KEY_L,KEY_Semicolon,KEY_Quotation,KEY_ENTER,0x00,KEY_R_CTRL},
+    {0x00,KEY_L_CTRL,0x00,0x00,KEY_S,KEY_C,KEY_V,KEY_N,KEY_M,KEY_COMMA,KEY_PERIOD,KEY_Interrogation,0x00,0x00,0x00},
+    {0x00,0x00,0x00,KEY_H,0x00,0x00,KEY_Y,KEY_B,0x00,0x00,KEY_LeftArrow,KEY_UpArrow,KEY_DownArrow,KEY_R_ALT,KEY_RightArrow},
+    {0x00,0x00,0x00,KEY_R_GUI,KEY_X,0x00,KEY_F4,KEY_G,0x00,0x00,KEY_SPACEBAR,0x00,0x00,KEY_L_ALT,KEY_Backspace},
+    {0x00,0x00,KEY_R_SHIFT,KEY_1,KEY_2,KEY_3,KEY_5,KEY_7,KEY_8,KEY_F8,KEY_F10,KEY_F12,'-',0x00,'/'},
+    {KEY_Delete,0x00,0x00,KEY_ESCAPE,KEY_F1,KEY_F2,KEY_F3,KEY_F5,KEY_F6,KEY_F7,KEY_F9,KEY_F11,'+',0x00,'*'}
 };
 
 void readscan(void);
